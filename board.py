c42697a613aae81d36fe6d7e4d13f6b73420b5c5
Thibaut Coutard <coutar_t@pc22.home>
2016-02-01 20:25:57 +0100

    board and piece already ok

---
+++ board.py
@@ -0,0 +1,62 @@
+import numpy as np
+import csv
+from piece import Piece
+
+class Board:
+
+    def __init__(self):
+        self.board = []
+        self.init_board()
+
+    # Initialize Board with random pieces
+    def init_board(self):
+        random_pieces = np.arange(16*16)
+        np.random.shuffle(random_pieces)
+        all_pieces = []
+        with open('project/e2pieces.txt', 'rb') as csvFile:
+            e2pieces = csv.reader(csvFile, delimiter=';', quotechar='|')
+            for row in e2pieces:
+                if (row[0] != "Top"):
+                    all_pieces.append(np.asarray(row).astype(int))
+        for i in random_pieces:
+            self.board.append(Piece(all_pieces[i], i))
+
+    # Get Note for the board
+    def notate(self):
+        self.note = 0
+        i = 0
+        for i in range(len(self.board)):
+            if (i > 16): #NOT TOP
+                if (random_pieces[i].faces[0] == random_pieces[i-16].faces[0]):
+                    self.note += 1
+            else: #TOP
+                self.note += 0
+            if (i < 240): #NOT BOT
+                if (random_pieces[i].faces[1] == random_pieces[i+16].faces[1]):
+                    self.note += 1
+            else: #BOT
+                self.note += 0
+            if (i % 16==0): #LEFT
+                if (random_pieces[i].faces[2] == random_pieces[i+1].faces[2]):
+                    self.note += 1
+            elif (i % 16==15): #RIGT
+                if (random_pieces[i].faces[3] == random_pieces[i-1].faces[3]):
+                    self.note += 1
+            else: #MIDDLE
+                if (random_pieces[i].faces[3] == random_pieces[i+1].faces[3]):
+                    self.note += 1
+                if (random_pieces[i].faces[2] == random_pieces[i-1].faces[2]):
+                    self.note += 1
+            i+=1
+    def printBoard(self):
+        i = 0
+        while i < len(self.board):
+            print str(self.board[i].piece) + " " + str(self.board[i+1].piece) + " " + str(self.board[i+2].piece) + " " + str(self.board[i+3].piece) + " " + str(self.board[i+4].piece) + " " + str(self.board[i+5].piece) \
+            + " " + str(self.board[i+6].piece) + " " + str(self.board[i+7].piece) + " " + str(self.board[i+8].piece) + " " + str(self.board[i+9].piece) + " " + str(self.board[i+10].piece) + " " + str(self.board[i+11].piece) \
+            + " " + str(self.board[i+12].piece) + " " + str(self.board[i+13].piece) + " " + str(self.board[i+14].piece)+ " " + str(self.board[i+15].piece) + "\n"
+            i+=16
+
+def main():
+    b = Board()
+    b.printBoard()
+main()
--- piece.py
+++ piece.py
@@ -1,66 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import numpy
+import numpy as np
 import collections
 
+## PIECE OF THE Board
+## self.piece is the id of the piece
+## self.faces is the array of the faces
 class Piece(object):
-	ALLOW_ANGLES = [0, 90, 180, 270]
-
-	FACES_ORIENTATION_POS = {
-		'top': 0,
-		'left': 3,
-		'right': 1,
-		'bottom': 2
-	}
-
-	def __init__(self, faces, piece_id, orientations, **options):
-		self.id = piece_id
-		self.rotation_angle = 0
-		self.actual_faces = self._set_faces(faces, orientations)
-		self.default_faces = self.actual_faces
-
-		self._is_corner = self._check_if_corner()
-		self._is_border = self._check_if_border()
-		self._picture_path = options.pop("picture_path", None)
-
-	def _set_faces(self, faces, orientations):
-		final_faces = numpy.zeros(len(orientations))
-		for i in range(len(orientations)):
-			numpy.put(final_faces, self.FACES_ORIENTATION_POS[orientations[i]], faces[i])
-		return final_faces
-
-	def is_border(self):
-		return self._is_border
-
-	def is_corner(self):
-		return self._is_corner
-
-	def _check_if_border(self):
-		counter = collections.Counter(self.default_faces)
-		if counter[0] > 0:
-			return True
-		return False
-
-	def _check_if_corner(self):
-		counter = collections.Counter(self.default_faces)
-		if counter[0] == 2:
-			return True
-		return False
-
-	def picture_path(self):
-		return self.picture_path
-
-	def rotate(self, angle):
-		if not angle in self.ALLOW_ANGLES:
-			print "Error : angle argument invalid."
-			return
-		self.rotation_angle = angle
-		if angle == 0:
-			self.actual_faces = self.default_faces
-		elif angle == 90:
-			self.actual_faces = numpy.roll(self.default_faces, 1)
-		elif angle == 180:
-			self.actual_faces = numpy.roll(self.default_faces, 2)
-		elif angle == 270:
-			self.actual_faces = numpy.roll(self.default_faces, 3)
+	def __init__(self, faces, piece, orient = 0):
+		self.faces = np.append(faces, orient)
+		self.piece = piece
