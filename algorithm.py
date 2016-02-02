f02a342c02fb82a7226e8b890d03c9b9a3d0301c
Thibaut Coutard <coutar_t@pc22.home>
2016-02-02 20:40:25 +0100

    add algorithm for now mutate and crossover only one element

--- algorithm.py
+++ algorithm.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import csv
+import csv, random, copy
 from piece import Piece
 from board import Board
 
@@ -19,28 +19,87 @@ class algorithm:
         self.best = self.boards[0]
         self.getBest()
 
+    # Init algo with numberOfBoards boards in the list
     def init_algo(self):
         for i in range(self.numberOfBoards):
             self.boards.append(Board())
 
+    # Get the best board
     def getBest(self):
         for i in self.boards:
             if (i.note > self.best.note):
                 self.best = i
 
+    # crossover between two boards and add it to the list
+    def crossoverOne(self, first, second):
+        num = random.randrange(0, 16*16-1)
+        t = second.board[num]
+        second2 = copy.copy(second)
+        first2 = copy.copy(first)
+        second2.life = random.range(7, 12)
+        first2.life = random.range(7, 12)
+        second2.change(num, first.board[num])
+        first2.change(num, t)
+        self.boards.append(second2)
+        self.boards.append(first)
+
+    # crossover
+    # we cross half population with a wheel selection
     def crossover(self):
+        numToCross = self.numberOfBoards / 2
+        sortedBoards = sorted(self.boards, key=lambda board: board.note, reverse=True)
+        maximum = 0
+        for i in sortedBoards:
+            maximum += i.note
+        for u in range(0, numToCross):
+            np = random.randrange(0,maximum)
+            first = sortedBoards[0]
+            second = sortedBoards[0]
+            for i in sortedBoards:
+                np -= i.note
+                if (np <= 0):
+                    np += maximum
+                    first = i
+            np = random.randrange(0,maximum)
+            for i in sortedBoards:
+                np -= i.note
+                if (i == first):
+                    np += i.note
+                if (np <= 0):
+                    np += maximum
+                    second = i
+            self.crossoverOne(first, second)
 
+    # mutate
+    # call mutate of the board
     def mutate(self):
+        n = random.randrange(0, len(self.boards)-1)
+        self.boards[n].mutate(random.randrange(0, 16*16-1))
 
+
+    # One Generation
+    # Call it for each generation
     def doOneGen(self):
+        # crossover
+
+        self.crossover()
+        # mutate
+        self.mutate()
+        maximum = 0
         for i in self.boards:
-            # crossover
-            self.crossover()
-            # mutate
-            self.mutate()
+            if (i.note > maximum):
+                maximum = i.note
+        print maximum
+        for i in self.boards:
             # substract life
             i.life -= 1
             # remove dead boards
             if (i.life == 0):
                 self.boards.remove(i)
         self.getBest()
+
+def main():
+    a = algorithm(4)
+    for i in range(0, 100):
+        a.doOneGen()
+main()
--- board.py
+++ board.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import csv
+import csv, random
 from piece import Piece
 
 ## BOARD OF THE GAME
@@ -31,6 +31,29 @@ class Board:
         for i in random_pieces:
             self.board.append(Piece(all_pieces[i], i))
 
+
+    # Mutate the element mut to a random mutated2
+    # We have to change the place where mutated2 was before with mutated1
+    def mutate(self, mut):
+        mutated1 = self.board[mut]
+        mutated2 = random.randrange(0, 16*16)
+        for i in range(0, len(self.board)):
+            if (self.board[i].piece == mutated2):
+                self.board[mut]=self.board[i]
+                self.board[i]=mutated1
+                mutated2 = -1
+        self.notate()
+
+    def change(self, num, numm):
+        tmp = self.board[num]
+        for i in range(0, len(self.board)):
+            if (self.board[i].piece == tmp):
+                self.board[num]=numm
+                self.board[i]=tmp
+                mutated2 = -1
+        self.notate()
+
+
     # Get Note for the board
     # We add +1 for each faces that match
     def notate(self):
@@ -38,25 +61,25 @@ class Board:
         i = 0
         for i in range(len(self.board)):
             if (i > 16): #NOT TOP
-                if (random_pieces[i].faces[0] == random_pieces[i-16].faces[0]):
+                if (self.board[i].faces[0] == self.board[i-16].faces[0]):
                     self.note += 1
             else: #TOP
                 self.note += 0
             if (i < 240): #NOT BOT
-                if (random_pieces[i].faces[1] == random_pieces[i+16].faces[1]):
+                if (self.board[i].faces[1] == self.board[i+16].faces[1]):
                     self.note += 1
             else: #BOT
                 self.note += 0
             if (i % 16==0): #LEFT
-                if (random_pieces[i].faces[2] == random_pieces[i+1].faces[2]):
+                if (self.board[i].faces[2] == self.board[i+1].faces[2]):
                     self.note += 1
             elif (i % 16==15): #RIGT
-                if (random_pieces[i].faces[3] == random_pieces[i-1].faces[3]):
+                if (self.board[i].faces[3] == self.board[i-1].faces[3]):
                     self.note += 1
             else: #MIDDLE
-                if (random_pieces[i].faces[3] == random_pieces[i+1].faces[3]):
+                if (self.board[i].faces[3] == self.board[i+1].faces[3]):
                     self.note += 1
-                if (random_pieces[i].faces[2] == random_pieces[i-1].faces[2]):
+                if (self.board[i].faces[2] == self.board[i-1].faces[2]):
                     self.note += 1
             i+=1
 
@@ -66,7 +89,7 @@ class Board:
         while i < len(self.board):
             print str(self.board[i].piece) + " " + str(self.board[i+1].piece) + " " + str(self.board[i+2].piece) + " " + str(self.board[i+3].piece) + " " + str(self.board[i+4].piece) + " " + str(self.board[i+5].piece) \
             + " " + str(self.board[i+6].piece) + " " + str(self.board[i+7].piece) + " " + str(self.board[i+8].piece) + " " + str(self.board[i+9].piece) + " " + str(self.board[i+10].piece) + " " + str(self.board[i+11].piece) \
-            + " " + str(self.board[i+12].piece) + " " + str(self.board[i+13].piece) + " " + str(self.board[i+14].piece)+ " " + str(self.board[i+15].piece) + "\n"
+            + " " + str(self.board[i+12].piece) + " " + str(self.board[i+13].piece) + " " + str(self.board[i+14].piece)+ " " + str(self.board[i+15].piece)
             i+=16
 
     def toString(self):
@@ -78,9 +101,3 @@ class Board:
             + " " + str(self.board[i+12].piece) + " " + str(self.board[i+13].piece) + " " + str(self.board[i+14].piece)+ " " + str(self.board[i+15].piece) + "\n"
             i+=16
         return mystr
-        
-
-def main():
-    b = Board()
-    b.printBoard()
-main()
