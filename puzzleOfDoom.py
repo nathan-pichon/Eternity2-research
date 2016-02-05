dc103359a2db9432fe4ae4a9fcdf0b796b8d4942
Coteazz <arthur.leclerc@epitech.eu>
2016-02-05 17:16:41 +0100

    UI update
    - Add tabulation (Generation & History)
    - Remove Load option
    - Preload eternity piece images
    - Add orientation

--- puzzleOfDoom.py
+++ puzzleOfDoom.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from Tkinter import *
+from ttk import *
 from tkMessageBox import *
 from PIL import ImageTk, Image
 
@@ -20,12 +21,42 @@ class PuzzleOfDoom:
         self.nbrGen = IntVar()
         self.nbrGen.set(1)
 
+        self.historyBestBoard = []
+
+        self.piecesIm = []
+
+    def initUI(self):
+
+        for i in range(1, 256):
+            self.piecesIm.append(Image.open("project/Eternity/" + str(i) + '.png'))
+            self.piecesIm[i - 1].thumbnail((25, 25), Image.ANTIALIAS)
+
+        self.attachBoardToCanvas(self.algorithm.best.board, self.canvasBestFrame)
+        self.attachBoardToCanvas(self.algorithm.boards[self.cursorPosition.get()].board, self.canvasBoardFrame)
+
+    def loadUI(self):
+        self.note = Notebook(self.windown)
+
+        self.loadTabGeneration()
+        self.loadTabHistory()
+        self.note.pack()
+
+    def loadTabGeneration(self):
+        self.tabGeneration = Frame(self.note)
+        self.loadBestFrame()
+        self.loadBoardFrame()
+        self.loadMenu()
+        self.note.add(self.tabGeneration, text="Generation")
+
+    def loadTabHistory(self):
+        self.tabHistory = Frame(self.note)
+        self.note.add(self.tabHistory, text="History")
+
     def loadMenu(self):
         menubar = Menu(self.windown)
 
         menu1 = Menu(menubar, tearoff=0)
         menu1.add_command(label="Save", command=self.save)
-        menu1.add_command(label="Load", command=self.load)
         menu1.add_separator()
         menu1.add_command(label="Quit", command=self.windown.quit)
         menubar.add_cascade(label="Fichier", menu=menu1)
@@ -38,7 +69,7 @@ class PuzzleOfDoom:
 
     def loadBestFrame(self):
         # Left side, show best board for the current generation
-        bestFrame = LabelFrame(self.windown, text=" Best board of the generation ", padx=20, pady=20)
+        bestFrame = LabelFrame(self.tabGeneration, text=" Best board of the generation ")
         bestFrame.pack(side=LEFT, padx=15, pady=15)
 
         self.canvasBestFrame = Canvas(bestFrame, width=400, height=400, background='black')
@@ -49,11 +80,11 @@ class PuzzleOfDoom:
 
         self.entree = Entry(bottomFrame, textvariable=self.nbrGen, width=10)
         self.entree.pack(side=LEFT)
-        Button(bottomFrame, text="Generate", relief=RAISED, command=self.doNextGen).pack(side=LEFT, padx=5, pady=5)
+        Button(bottomFrame, text="Generate", command=self.doNextGen).pack(side=LEFT, padx=5, pady=5)
 
     def loadBoardFrame(self):
         # Right side, show all boards for the current generation
-        boardsFrame = LabelFrame(self.windown, text=" Boards ", padx=20, pady=20)
+        boardsFrame = LabelFrame(self.tabGeneration, text=" Boards ")
         boardsFrame.pack(side=RIGHT, padx=15, pady=15)
 
         self.canvasBoardFrame = Canvas(boardsFrame, width=400, height=400, background='black')
@@ -62,12 +93,12 @@ class PuzzleOfDoom:
         bottomFrame = Frame(boardsFrame, relief=FLAT, borderwidth=1)
         bottomFrame.pack(side=BOTTOM, padx=5, pady=5)
 
-        Button(bottomFrame, text="<<", relief=RAISED, command=self.PreviousBoard).pack(side=LEFT, padx=5, pady=5)
+        Button(bottomFrame, text="<<",command=self.PreviousBoard).pack(side=LEFT, padx=5, pady=5)
         Label(bottomFrame, textvariable=self.cursorPosition).pack(side=LEFT, padx=5, pady=5)
         self.boardCount.set(len(self.algorithm.boards) - 1)
         Label(bottomFrame, text='/').pack(side=LEFT, padx=5, pady=5)
         Label(bottomFrame, textvariable=self.boardCount).pack(side=LEFT, padx=5, pady=5)
-        Button(bottomFrame, text=">>", relief=RAISED, command=self.NextBoard).pack(side=LEFT, padx=5, pady=5)
+        Button(bottomFrame, text=">>", command=self.NextBoard).pack(side=LEFT, padx=5, pady=5)
 
     def about(self):
         showinfo("About", "Puzzle Of Doom - 2016, Epitech Project by:\n\nNathan Pichonwalchshofer\tpichon_b\nThibaut Coutard\t\tcoutar_t\nAurelien Dorey\t\tdorey_a\nArthur Leclerc\t\tlecler_h")
@@ -114,10 +145,7 @@ class PuzzleOfDoom:
         y = 0
 
         for piece in board:
-            pieceIm = Image.open("project/Eternity/" + str(piece.piece + 1) + '.png')
-            pieceIm.thumbnail((25, 25), Image.ANTIALIAS)
-
-            im.paste(pieceIm, (x, y))
+            im.paste(self.piecesIm[piece.piece - 1].rotate(piece.orientation), (x, y))
             x += 25
             if i % 16 == 0:
                 y += 25
@@ -130,12 +158,8 @@ class PuzzleOfDoom:
 def main():
     puzzleOfDoom = PuzzleOfDoom()
 
-    puzzleOfDoom.loadBestFrame()
-    puzzleOfDoom.loadBoardFrame()
-    puzzleOfDoom.loadMenu()
-
-    puzzleOfDoom.attachBoardToCanvas(puzzleOfDoom.algorithm.best.board, puzzleOfDoom.canvasBestFrame)
-    puzzleOfDoom.attachBoardToCanvas(puzzleOfDoom.algorithm.boards[puzzleOfDoom.cursorPosition.get()].board, puzzleOfDoom.canvasBoardFrame)
+    puzzleOfDoom.loadUI()
+    puzzleOfDoom.initUI()
 
     puzzleOfDoom.windown.mainloop()
 
